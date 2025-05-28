eee14a6ab84b49a8389d88c717ce52b84743feac
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-05-28 09:03:42 +0530

    New Class Created

---
+++ Cucumber_ERP/src/test/java/commonFunctions/Sample.java
@@ -0,0 +1,25 @@
+package commonFunctions;
+
+public class Sample {
+public void add()
+{
+	int a=8766;
+	int b=45;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=5656;
+	int b=6;
+	int c=a/b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=7676;
+	int b=3;
+	int c=a*b;
+	System.out.println(c);
+}
+}
